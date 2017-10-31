702416ab5bac22139e0b79487ca37548c94a0f01
Fredrik Sandström <Fredrik.Sandstrom@triona.se>
2017-10-31 15:37:04 +0100

    Added Entity Framework

--- PieShop/Startup.cs
+++ PieShop/Startup.cs
@@ -1,23 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using PieShop.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 namespace PieShop
 {
   public class Startup
   {
+    private IConfigurationRoot _configurationRoot;
+
+    public Startup(IHostingEnvironment hostingEnvironment)
+    {
+      _configurationRoot = new ConfigurationBuilder()
+        .SetBasePath(hostingEnvironment.ContentRootPath)
+        .AddJsonFile("appsettings.json")
+        .Build();
+    }
     // This method gets called by the runtime. Use this method to add services to the container.
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services)
     {
-      services.AddTransient<IPieRepository, MockPieRepository>();
-      services.AddTransient<ICategoryRepository, MockCategoryRepository>();
+      services.AddDbContext<AppDbContext>(options => options.UseSqlServer(_configurationRoot.GetConnectionString("DefaultConnection")));
+      //services.AddTransient<IPieRepository, MockPieRepository>();
+      //services.AddTransient<ICategoryRepository, MockCategoryRepository>();
+      services.AddTransient<IPieRepository, PieRepository>();
+      services.AddTransient<ICategoryRepository, CategoryRepository>();
       services.AddMvc();
     }
 
